e69ce700adc7d79d3d0c48a6e27b7f2aeacddf97
mythicalbadger <jussy12.work@gmail.com>
2023-11-20 20:00:51 +0700

    fix: comments

--- src/db_services.py
+++ src/db_services.py
@@ -8,10 +8,16 @@ DEFAULT_TOKEN_AMOUNT = 100
 
 
 def create_database() -> None:
-    return _database.init_db()
+    """
+    Initializes the database engine
+    """
+    _database.init_db()
 
 
 def populate_inventory() -> None:
+    """
+    Populates the inventory table with tokens
+    """
     with Session(engine) as session:
         tokens = [
             Inventory(token_type=token_type.value, amount=DEFAULT_TOKEN_AMOUNT)
