19148247ee80dcc5c3c0ab58c2b8cb5648675d02
mythicalbadger <jussy12.work@gmail.com>
2023-11-24 11:07:27 +0700

    fix: remove unused session for order creation

--- src/routers/orders.py
+++ src/routers/orders.py
@@ -1,8 +1,6 @@
 from src.redis import RedisResource as redis
 from src.redis import Queue
 
-from src.database import get_session
-from sqlmodel import Session
 from fastapi import APIRouter, Depends
 from fastapi_jwt_auth import AuthJWT
 from src.models import OrderInformation
@@ -16,7 +14,6 @@ router = APIRouter(tags=["users"])
 async def create_order(
     order: OrderInformation,
     Authorize: AuthJWT = Depends(),
-    session: Session = Depends(get_session),
 ):
     Authorize.jwt_required()
 
