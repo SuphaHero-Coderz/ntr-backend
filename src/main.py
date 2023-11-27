06f25643703a2c3cdedc863670c5a56edb690364
mythicalbadger <jussy12.work@gmail.com>
2023-11-27 20:48:45 +0700

    fix: removing telemetry for now...

--- src/main.py
+++ src/main.py
@@ -6,31 +6,9 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi_jwt_auth.exceptions import AuthJWTException
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-)
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
 app = FastAPI()
 
-trace.set_tracer_provider(
-    TracerProvider(resource=Resource.create({SERVICE_NAME: "ntr-backend"}))
-)
-
-jaeger_exporter = JaegerExporter(
-    agent_host_name="jaeger",
-    agent_port=6831,
-)
-
-trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(jaeger_exporter))
-
-
-FastAPIInstrumentor.instrument_app(app)
-tracer = trace.get_tracer(__name__)
 _services.create_database()
 
 load_dotenv()
--- src/routers/orders.py
+++ src/routers/orders.py
@@ -1,5 +1,4 @@
 import src.db_services as _services
-import logging
 from src.redis import RedisResource as redis
 from src.redis import Queue
 
@@ -8,14 +7,8 @@ from fastapi_jwt_auth import AuthJWT
 from src.models import OrderInformation, User
 from sqlmodel import Session
 from src.database import get_session
-from opentelemetry import trace
 
 router = APIRouter(tags=["users"])
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-tracer = trace.get_tracer("backend.tracer")
 
 
 @router.post("/create-order", status_code=201)
@@ -33,15 +26,11 @@ async def create_order(
     user_id: int = Authorize.get_jwt_subject()
     user: User = await _services.get_user_by_id(user_id, session)
 
-    with tracer.start_as_current_span("order_create") as orderspan:
-        survival_bag = {
-            "user_id": user_id,
-            "user_credits": user.credits,
-            "num_tokens": order.num_tokens,
-        }
-        orderspan.add_event("Order created")
-        orderspan.set_attribute("order.info", survival_bag)
-        logger.info("Pushing order info to Order Creation service: ", survival_bag)
-        redis.push_to_queue(Queue.order_queue, survival_bag)
+    survival_bag = {
+        "user_id": user_id,
+        "user_credits": user.credits,
+        "num_tokens": order.num_tokens,
+    }
+    redis.push_to_queue(Queue.order_queue, survival_bag)
 
-        return {"message": "Order created"}
+    return {"message": "Order created"}
